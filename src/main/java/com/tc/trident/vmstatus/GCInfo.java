1545d9d22dd4b5b3f750aef580255e31e392f031
高峰 <gaofeng@eetop.com>
2015-01-20 09:38:31 +0800

    daily commit

---
+++ src/main/java/com/tc/trident/vmstatus/GCInfo.java
@@ -0,0 +1,47 @@
+
+package com.tc.trident.vmstatus;
+
+/**
+ *
+ * @author kozz.gaof
+ * @date Jan 19, 2015 7:11:34 PM
+ * @id $Id$
+ */
+public class GCInfo {
+    
+    private String name;
+    
+    private long count;
+    
+    private long time;
+    
+    public String getName() {
+    
+        return name;
+    }
+    
+    public void setName(String name) {
+    
+        this.name = name;
+    }
+    
+    public long getCount() {
+    
+        return count;
+    }
+    
+    public void setCount(long count) {
+    
+        this.count = count;
+    }
+    
+    public long getTime() {
+    
+        return time;
+    }
+    
+    public void setTime(long time) {
+    
+        this.time = time;
+    }
+}
